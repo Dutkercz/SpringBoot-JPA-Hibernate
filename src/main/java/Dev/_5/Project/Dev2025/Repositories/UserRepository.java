cde3d9f74d0d558da88b5a8261323d7c180e1141
dutkercz <dutkercz@gmail.com>
2025-01-16 01:58:49 -0300

    Create Jpa repository, db seeding.

---
+++ src/main/java/Dev/_5/Project/Dev2025/Repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package Dev._5.Project.Dev2025.Repositories;
+
+import Dev._5.Project.Dev2025.Entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
