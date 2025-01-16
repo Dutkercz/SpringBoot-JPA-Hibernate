f0870c5300a8ec4c1bd8dd6e6dd01f8c740a76c3
dutkercz <dutkercz@gmail.com>
2025-01-16 01:02:00 -0300

    Fisrt Commit

---
+++ src/test/java/Dev/_5/Project/Dev2025/Dev2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package Dev._5.Project.Dev2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dev2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
