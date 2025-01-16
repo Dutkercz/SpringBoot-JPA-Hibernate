18015cd2fbacc03752c6dc5a710745ed9369d749
dutkercz <dutkercz@gmail.com>
2025-01-16 01:08:56 -0300

    ......

---
+++ src/main/java/Dev/_5/Project/Dev2025/User.java
@@ -0,0 +1,9 @@
+package Dev._5.Project.Dev2025;
+
+public class User {
+    private Integer id;
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+}
